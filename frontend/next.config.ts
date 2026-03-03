cd55a23f7f0f294e3215f4a0b63e6be94dbc70e7
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-03-03 10:21:25 +0530

    origin-issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    'local-origin.dev',
+    '*.local-origin.dev',
+    '192.168.1.*', // Some versions support IP wildcards
+    'localhost:3000'
+  ],
 };
 
 export default nextConfig;
